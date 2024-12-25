e3701aede19b04c49a08afe096dbb158d911c831
khmerweb <vuthdevelop1@gmail.com>
2024-12-25 21:37:08 +0700

    Make it better

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,11 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		/*
+		
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-		*/
+		
 	}
 }
 
